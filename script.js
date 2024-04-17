0899434ead7d1d69f4eef0884ed717657c6dfc71
Jordan J <jordanr2m@gmail.com>
2024-04-17 13:10:50 -0700

    Initial commit with HTML.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Populate Copyright year
+document.querySelector("#year").innerText = new Date().getFullYear();
+
+const dogsArray = [
+    {
+        name: "Pomeranian",
+        src: "",
+        alt: "Pomeranian",
+        energy: "Low",
+        home: "Apartment",
+        size: "small",
+        description: "Pomeranians are spunky, cuddly, and lovable. They are great dogs for beginners and an excellent choice for apartments and small homes."
+    }
+]

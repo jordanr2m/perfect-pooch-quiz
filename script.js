1e93c9dc32160be9d724731a78c93faed0028a30
Jordan J <jordanr2m@gmail.com>
2024-04-18 14:41:52 -0700

    Worked on functoinality for Advanced Search options.

--- script.js
+++ script.js
@@ -2,17 +2,28 @@
 document.querySelector("#year").innerText = new Date().getFullYear();
 
 // Select elements
+const introHeading = document.querySelector(".intro h2");
 const randomBtn = document.querySelector(".random");
 const resetBtn = document.querySelector(".reset");
+const resetSpan = document.querySelector(".reset-span");
 const mainImg = document.querySelector(".main-img");
 const pickAPup = document.querySelector(".pick-a-pup");
 const dogDisplay = document.querySelector(".dog-display");
+const noMatch = document.querySelector(".no-match");
 
 // Display variables
 const dogName = document.querySelector(".name");
 const dogImg = document.querySelector("#dog-img");
 const energyLevel = document.querySelector(".energy-level");
+const size = document.querySelector(".size");
 const description = document.querySelector(".description");
+
+// Advanced Search elements
+const advancedSearch = document.querySelector(".advanced-search");
+const searchBtn = document.querySelector(".search");
+const energyInput = document.querySelector("#energy");
+const homeInput = document.querySelector("#home");
+const sizeInput = document.querySelector("#size");
 
 const dogsArray = [
     {
@@ -22,27 +33,81 @@ const dogsArray = [
         energy: "Low",
         home: "Apartment",
         size: "Small",
-        description: "Pomeranians are spunky, cuddly, and lovable. They are great dogs for beginners and are also an excellent choice for apartments and small homes."
+        description: "Pomeranians are spunky, cuddly, and lovable. They are great dogs for beginners because they are both easy to train and loyal. Pomeranians are also an excellent choice for those living in apartments or small homes."
     }
 ]
 
-function randomDog () {
+function randomDog() {
     const dogIndex = Math.floor(Math.random() * dogsArray.length);
     let randomDog = dogsArray[dogIndex];
     disaplyRandomDog(randomDog);
 }
 
-function disaplyRandomDog (dog) {
-    resetBtn.hidden = false;
-    mainImg.hidden = true;
-    pickAPup.hidden = true;
-    dogDisplay.hidden = false;
+function updateDogInfo (dog) {
     dogName.innerText = dog.name;
     dogImg.src = dog.src;
     dogImg.alt = dog.alt;
     energyLevel.innerText = dog.energy;
+    size.innerText = dog.size;
     description.innerText = dog.description;
 }
 
+function disaplyRandomDog (dog) {
+    resetBtn.hidden = false;
+    dogDisplay.hidden = false;
+    mainImg.hidden = true;
+    pickAPup.hidden = true;
+    updateDogInfo(dog);
+}
+
+function resetSearch() {
+    window.location.reload();
+}
+
+function showAdvancedSearch() {
+    advancedSearch.hidden = false;
+    searchBtn.hidden = false;
+    randomBtn.hidden = true;
+}
+
+function advancedDogSearch() {
+    let selectedEnergy = energyInput.value;
+    let selectedHome = homeInput.value;
+    let selectedSize = sizeInput.value;
+
+    dogsArray.forEach(dog => {
+        if (dog.energy.includes(selectedEnergy) && dog.home.includes(selectedHome) && dog.size.includes(selectedSize)) {
+            // Create a copy of dogDisplay div
+            let clone = dogDisplay.cloneNode(true);
+            dogDisplay.after(clone);
+            // Populate div information
+            updateDogInfo(dog);
+            // Update UI
+            dogDisplay.hidden = false;
+            resetBtn.hidden = false;
+            introHeading.hidden = true;
+            randomBtn.hidden = true;
+            searchBtn.hidden = true;
+            mainImg.hidden = true;
+            pickAPup.hidden = true;
+            advancedSearch.hidden = true;
+            searchBtn.hidden = true;
+        } else {
+            noMatch.hidden = false;
+        }
+    })
+};
+
+function removeNoMatch() {
+    noMatch.hidden = true;
+}
 
 randomBtn.addEventListener("click", randomDog);
+resetBtn.addEventListener("click", resetSearch)
+resetSpan.addEventListener("click", resetSearch);
+energyInput.addEventListener("change", showAdvancedSearch);
+searchBtn.addEventListener("click", advancedDogSearch);
+
+energyInput.addEventListener("change", removeNoMatch);
+homeInput.addEventListener("change", removeNoMatch);
+sizeInput.addEventListener("change", removeNoMatch);
